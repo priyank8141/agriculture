94329d7f20759ebda925df06f3fd52994b3e763b
priyank8141 <jainpriyank789@gmail.com>
2020-12-05 12:47:04 +0530

    final view page

--- agrifront/src/app/main/main.module.ts
+++ agrifront/src/app/main/main.module.ts
@@ -3,12 +3,13 @@ import { CommonModule } from '@angular/common';
 import { HeaderComponent } from './header/header.component';
 import { ViewComponent } from './view/view.component';
 import { FooterComponent } from './footer/footer.component';
+import { LoginComponent } from './login/login.component';
 
 
 
 @NgModule({
-  declarations: [HeaderComponent, ViewComponent, FooterComponent],
-  exports: [HeaderComponent,ViewComponent],
+  declarations: [HeaderComponent, ViewComponent, FooterComponent, LoginComponent],
+  exports: [HeaderComponent,ViewComponent,LoginComponent],
   imports: [
     CommonModule
   ]
